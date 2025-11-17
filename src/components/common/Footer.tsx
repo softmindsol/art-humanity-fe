b0c3bf2fed0c16e7a18dff5856b1c046a2c62984
arfan4-dev <arfan4.dev@gmail.com>
2025-11-17 13:12:33 +0500

    Set teh social media Icons

--- src/components/common/Footer.tsx
+++ src/components/common/Footer.tsx
@@ -1,18 +1,36 @@
 import { Link } from "react-router-dom";
+import { FaYoutube, FaTiktok, FaInstagram, FaDiscord } from 'react-icons/fa';
 
-const Footer = () => { 
+const Footer = () => {
   return (
     <footer className="flex flex-col justify-end ">
 
 
       <div className="flex flex-col lg:flex-row justify-center items-start  lg:gap-[13.5rem] xl:gap-[28.5rem]">
-      
-        <span  className="logo-link-footer">
+
+        <span className="logo-link-footer">
           <img src="/favicon.PNG" alt="Logo" className="logo -ml-4 md:-ml-0 lg:-ml-4" />
-          <div className=" ">
-            <h1 className='text-[1.8rem] sm:!text-[2rem] !text-[#efebe9]  '>MurArt</h1>
-            <p className="text-[14px] md:text-[16px] !italic !text-[#efebe9]">Collaborative Canvases of Human Expression</p>
+          <div>
+            <div className=" ">
+              <h1 className='text-[1.8rem] sm:!text-[2rem] !text-[#efebe9]  '>MurArt</h1>
+              <p className="text-[14px] md:text-[16px] !italic !text-[#efebe9]">Collaborative Canvases of Human Expression</p>
+            </div>
+            <div className="flex my-4">
+              <a href="https://www.youtube.com/@MurArtio" target="_blank" rel="noopener noreferrer" className="!text-white mr-4 text-2xl hover:text-gray-400">
+                <FaYoutube />
+              </a>
+              <a href="https://www.tiktok.com/@murart.io" target="_blank" rel="noopener noreferrer" className="!text-white mx-4 text-2xl hover:text-gray-400">
+                <FaTiktok />
+              </a>
+              <a href="https://www.instagram.com/murart.io" target="_blank" rel="noopener noreferrer" className="!text-white mx-4 text-2xl hover:text-gray-400">
+                <FaInstagram />
+              </a>
+              <a href="https://discord.gg/ZGWB9zZF" target="_blank" rel="noopener noreferrer" className="!text-white mx-4 text-2xl hover:text-gray-400">
+                <FaDiscord />
+              </a>
+            </div>
           </div>
+
         </span>
         <div className="flex lg:!items-start md:gap-14  mb-2 ">
           <div className="footer-column ">
