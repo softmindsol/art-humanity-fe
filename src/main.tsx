213d035fc5b091df5c96cdb1b8c86c00178f6e51
Huzaifa-Solutions <softmindsol@gmail.com>
2026-02-10 12:04:55 +0500

    feat: initialize React application with Redux, Redux Persist, and Sonner toast configuration.

--- src/main.tsx
+++ src/main.tsx
@@ -6,6 +6,7 @@ import "./index.css";
 import { store, persistor } from "./redux/store/index.ts"; // ✅ import persistor
 import { PersistGate } from "redux-persist/integration/react";
 import { Toaster } from "sonner";
+import { CircleAlert, CircleCheckBig } from "lucide-react";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
@@ -32,37 +33,13 @@ createRoot(document.getElementById("root")!).render(
           }}
           icons={{
             success: (
-              <div className="w-6 h-6 rounded-full bg-[#5D4037] flex items-center justify-center">
-                <svg
-                  width="14"
-                  height="14"
-                  viewBox="0 0 24 24"
-                  fill="none"
-                  stroke="white"
-                  strokeWidth="3"
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                >
-                  <polyline points="20 6 9 17 4 12" />
-                </svg>
+              <div className="w-6 h-6 rounded-full flex items-center justify-center">
+                <CircleCheckBig color="white" className="!mr-1" />
               </div>
             ),
             error: (
-              <div className="w-6 h-6 rounded-full bg-[#fff] flex items-center justify-center">
-                <svg
-                  width="24"
-                  height="24"
-                  viewBox="0 0 24 24"
-                  fill="none"
-                  stroke="black"
-                  strokeWidth="3"
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  className="w-3.5 h-3.5"
-                >
-                  <line x1="12" y1="8" x2="12" y2="12"></line>
-                  <line x1="12" y1="16" x2="12.01" y2="16"></line>
-                </svg>
+              <div className="w-6 h-6 rounded-full flex items-center justify-center">
+                <CircleAlert color="white" className="!mr-1" />
               </div>
             ),
           }}
