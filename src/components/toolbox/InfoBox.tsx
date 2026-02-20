4e12f35e07ffed31517ed0e6530ae99621433e2a
Huzaifa-Solutions <softmindsol@gmail.com>
2026-02-20 11:05:26 +0500

    feat: Add draggable InfoBox component to display canvas data and a Toolbox for drawing controls.

--- src/components/toolbox/InfoBox.tsx
+++ src/components/toolbox/InfoBox.tsx
@@ -5,7 +5,7 @@ import React, {
   useCallback,
   useMemo,
 } from "react";
-import { Minus, Plus } from "lucide-react";
+import { List, Minus, Plus } from "lucide-react";
 import { selectCanvasData } from "@/redux/slice/contribution";
 import { useSelector } from "react-redux";
 import { useMediaQuery } from "@/hook/useMediaQuery";
@@ -160,23 +160,7 @@ const InfoBox = ({ zoom, worldPos, isSaving, saveError, boundaryRef }: any) => {
         onTouchStart={handleDragTouchStart}
       >
         <div className="cursor-grab active:cursor-grabbing text-[#ffffff]">
-          <svg
-            width="24"
-            height="24"
-            viewBox="0 0 24 24"
-            fill="none"
-            stroke="currentColor"
-            strokeWidth="2"
-            strokeLinecap="round"
-            strokeLinejoin="round"
-          >
-            <circle cx="9" cy="12" r="1"></circle>
-            <circle cx="9" cy="5" r="1"></circle>
-            <circle cx="9" cy="19" r="1"></circle>
-            <circle cx="15" cy="12" r="1"></circle>
-            <circle cx="15" cy="5" r="1"></circle>
-            <circle cx="15" cy="19" r="1"></circle>
-          </svg>
+          <List />
         </div>
         <p className="text-[#ffffff] text-base font-medium m-0">Infobox</p>
         <div className="flex text-[#ffffff] items-center gap-2">
