6a40fef7347f4a8bc58a5d0a60b085ffcac9ea56
Huzaifa-Solutions <huzaifaihsan002@gmail.com>
2026-02-25 15:25:13 +0500

    feat: Create ArtworkShowcaseSection component and add pastoral-stillness artwork image.

--- src/components/artwork-showcase/ArtworkShowcaseSection.tsx
+++ src/components/artwork-showcase/ArtworkShowcaseSection.tsx
@@ -6,21 +6,21 @@ const ArtworkShowcaseSection: React.FC = () => {
       {/* Background Image */}
       <div className="absolute inset-0">
         <img
-          src="/assets/goat.svg"
+          src="/assets/pastoral-stillness.webp"
           alt="Pastoral Stillness"
-          className="w-full h-full object-cover"
+          className="w-full h-full object-cover opacity-65"
         />
         {/* Gradient Overlay for Text Readability */}
       </div>
 
       <div className="max-w-[1440px] px-6 2xl:px-8 mx-auto h-full flex items-end pb-8 lg:pb-16 relative z-10">
         <div className="max-w-3xl md:space-y-6 space-y-4">
-          <p className="!text-white text-sm md:text-base font-medium">
+          <p className="!text-white text-sm md:text-base font-semibold">
             You can create artwork of this type and also you can contribute{" "}
             <br className="hidden md:block" /> to other people artworks
           </p>
 
-          <h2 className="text-[24px] lg:text-[34px] !font-semibold !text-white leading-[1.1]">
+          <h2 className="text-[24px] lg:text-[34px] font-semibold !text-white leading-[1.1]">
             Pastoral Stillness with Sheep <br />
             Anonymous{" "}
             <span className="bg-gradient-to-r from-[#E23373] to-[#FEC133] bg-clip-text text-transparent">
